0379a32591ff4db9653f0c3929fbf90842ff0627
RtYkk <45971730+RtYkk@users.noreply.github.com>
2025-09-28 09:16:19 +0800

    feat: Initialize Android project structure with basic setup
    - Added AndroidManifest.xml for application configuration.
    - Created MainActivity.kt with a simple greeting UI using Jetpack Compose.
    - Implemented color and typography themes in the ui.theme package.
    - Added drawable resources for app icons and backgrounds.
    - Included example instrumented and unit tests.
    - Configured Gradle build files and properties for project dependencies.
    - Established project-wide settings in settings.gradle.kts.

---
+++ android/app/src/main/java/jlu/kemiko/libman/MainActivity.kt
@@ -0,0 +1,47 @@
+package jlu.kemiko.libman
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import jlu.kemiko.libman.ui.theme.LibmanTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LibmanTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LibmanTheme {
+        Greeting("Android")
+    }
+}
